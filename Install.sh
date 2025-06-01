22bd840476361607fcee5ab650eb6cd7617680b9
Raj singh chauhan <rajsinghchauhan352@gmail.com>
2025-06-01 22:33:22 +0000

    fixed the installer

--- Install.sh
+++ Install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Install Hyprland and its dependencies on Arch Linux
 # This script assumes you have sudo privileges and the necessary repositories enabled.
-$config = ~/.config
 
-sudo pacman -S hyprland hypridle hyprlock wlogout libnotify brightnessctl playerctl wofi rofi wezterm kitty polkit-gnome python-pywal16 power-profiles-daemon noto-fonts-emoji ttf-fira-code ttf-font-awesome swww hyprpaper grim slurp stead blueman 
+config="$HOME/.config"
+
+sudo pacman -S hyprland hypridle hyprlock wlogout libnotify brightnessctl playerctl wofi rofi wezterm kitty polkit-gnome power-profiles-daemon noto-fonts-emoji ttf-fira-code ttf-font-awesome swww hyprpaper grim slurp blueman
+
 hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
 hyprpm enable dynamic-cursors
 
-cp -r hypr "$config/"
-cp -r fastfetch "$config/"
-cp -r rofi "$config/"
-cp -r wofi "$config/"
-cp -r wal "$config/"
-cp -r waybar "$config/"
-cp -r wlogout "$config/"
-cp -r dunst "$config/"
-cp -r wezterm "$config/"
-cp -r swaync "$config/"
+for dir in hypr fastfetch rofi wofi wal waybar wlogout dunst wezterm swaync; do
+  src_dir="$PWD/$dir"
+  dest_dir="$config/$dir"
+  if [ -d "$src_dir" ]; then
+    mkdir -p "$dest_dir"
+    cp -rf "$src_dir/"* "$dest_dir/"
+  else
+    echo "Warning: $src_dir not found, skipping."
+  fi
+done
